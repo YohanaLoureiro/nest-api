d49a1b70595f244eba3340544f3b9e5688727cfe
Yohana Dhara <yohanaloureiro@gmail.com>
2023-02-27 20:40:32 -0300

    Ajuste - conexao banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonController } from './controller/person.controller';
+import { PersonService } from './service/person.service';
+import { TaskController } from './controller/task.controller';
+import { TaskService } from './service/task.service';
+import { Person } from './entities/person.entity';
+import { Task } from './entities/task.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [Person, Task],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Person, Task]),
+  ],
+  controllers: [AppController, PersonController, TaskController],
+  providers: [AppService, PersonService, TaskService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
